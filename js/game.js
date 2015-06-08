96bf1cbb0c431d1043910355823d39e5a5f68f43
Nikolay Ignatyev <noilgt@gmail.com>
2015-06-08 13:14:17 +0300

    added mousedown event

--- js/game.js
+++ js/game.js
@@ -1,10 +1,22 @@
 $(document).ready(function() {
     alert($(".boardCell img").length + ' figures!');
 
+    // скрывание фигуры при наведении мыши
     $(".boardCell img").mouseenter(function() {
         $(this).fadeTo('fast', 0);
         //alert($("img").attr("src"));
     });
+
+    // удаление фигуры по клику
+    $(".boardCell img").mousedown(function() {
+    	var figure = $(this);
+    	$(this).remove();
+    });
+
+    //$(".boardCell").mousedown(function() {
+    //	var figure = $(this);
+    //	$(this).remove();
+    //});
     
     $(".boardCell img").mouseleave(function() {
         $(this).fadeTo('fast', 1);
